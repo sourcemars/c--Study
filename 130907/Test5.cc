40f587d775afcb5364b2845d715f49acf44d7e4a
mars <source.mars@gmail.com>
2013-09-08 00:32:26 +0800

    第二个版本的Test5.cc文件，主要为了循环读入\字符串

--- 130907/Test5.cc
+++ 130907/Test5.cc
@@ -7,7 +7,10 @@ using std::cin;
 using std::endl;
 int main(){ //前几个练习当中写的主函数好像没有写return，这也行？
   string s;
-  cin >> s;
-  cout << s << endl;
+  /* cin >> s; */
+  /* cout << s << endl; */
+  while(cin >> s){//我擦，读入一个字符串竟然还有返回值，悲催，还不知道怎么看c++的文档，导致看不明白。。。
+    cout << s << endl;
+  }
   return 0;
 }
