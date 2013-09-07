3837f18ba469812e177b6dfe05239b8175930519
mars <source.mars@gmail.com>
2013-09-08 00:23:30 +0800

    第一个版本的Test5.cc文件，主要为了测试读入一个字符串:w

---
+++ 130907/Test5.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+int main(){ //前几个练习当中写的主函数好像没有写return，这也行？
+  string s;
+  cin >> s;
+  cout << s << endl;
+  return 0;
+}
