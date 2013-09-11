5385cbd03a6b0650dc2e9474526d0a73985d7daa
mars <source.mars@gmail.com>
2013-09-11 22:51:33 +0800

    test

---
+++ 130911/Test1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+int main(){
+  string st("The expense of spirite\n");
+
+  cout << "size " << st.size() << endl;
+  return 0;
+}
